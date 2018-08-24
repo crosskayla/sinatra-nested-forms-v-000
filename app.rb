806151e714c57dbbf5c9cd7a32e66557b261c168
Kayla Cross <kac288@georgetown.edu>
2018-08-24 19:20:03 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -6,7 +6,11 @@ module FormsLab
   class App < Sinatra::Base
 
     get '/' do
-      erb :new
+      erb :root
+    end
+
+    get '/new' do
+      erb :'pirates/new'
     end
 
     post '/pirates' do
